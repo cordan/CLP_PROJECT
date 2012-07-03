7b51e606fa0aaf2dbc04cad4ec595c649b3f5d3d
Roberto Marroquin <Roberto Marroquin@157.158.160.194>
2012-07-03 22:20:15 +0200

    beta 600

--- src/src/AntsCriticalPath.java
+++ src/src/AntsCriticalPath.java
@@ -5,13 +5,11 @@
 package src;
 
 import JaCoP.constraints.netflow.NetworkBuilder;
+import JaCoP.constraints.netflow.NetworkFlow;
 import JaCoP.constraints.netflow.simplex.Arc;
 import JaCoP.constraints.netflow.simplex.Node;
 import JaCoP.core.IntVar;
 import JaCoP.core.Store;
-
-//dupadupa!
-
 
 
 /**
@@ -20,45 +18,56 @@ import JaCoP.core.Store;
  */
 public class AntsCriticalPath {
     
-      Store store = new Store();
+    Store store = new Store();
+
+    int num_arcs = 10;
+    int num_nodes=6;
+    int[] weights = {5,15,3,1,2,1,9,5,10,7}; 
+
+
+
+    NetworkBuilder net = new NetworkBuilder(); 
+
+
+    public void Network_definition() {
+        Node nodes[] =new Node[num_nodes];  
+        Arc arcs[] =new Arc[num_arcs];
+
+        nodes[0] = net.addNode("one", 0); 
+        nodes[1] = net.addNode("two", 0); 
+        nodes[2] = net.addNode("three", 0); 
+        nodes[3] = net.addNode("four", 0); 
+        nodes[4] = net.addNode("five", 0); 
+        nodes[5] = net.addNode("six", 0); 
+
+
+        arcs[0] = net.addArc(nodes[0], nodes[1], weights[0]); 
+        arcs[1] = net.addArc(nodes[0],nodes[3], weights[1]);
+        arcs[2] = net.addArc(nodes[0],nodes[2], weights[2]);
+        arcs[3] = net.addArc(nodes[1], nodes[2], weights[3]); 
+        arcs[4] = net.addArc(nodes[1],nodes[3], weights[4]);
+        arcs[5] = net.addArc(nodes[1],nodes[4], weights[5]); 
+        arcs[6] = net.addArc(nodes[2],nodes[4], weights[6]);
+        arcs[7] = net.addArc(nodes[3],nodes[4], weights[7]); 
+        arcs[8] = net.addArc(nodes[3],nodes[5], weights[8]);
+        arcs[9] = net.addArc(nodes[4],nodes[5], weights[9]);
+
+
+        IntVar cost = new IntVar(store, "cost", 0, 50);     
+        net.setCostVariable(cost);        
+        store.impose(new NetworkFlow(net));
+
+    }
+
+    public void search_solution(){
+
+        //SelectChoicePoint select = new SimpleSelect();
+        //SimpleSelect select = new SimpleSelect(store,);
+
+      }
       
-      int connections = 10;
-      int nodes=6;
-      int[] weights = {5,15,3,1,2,1,9,5,10,7}; 
       
-      IntVar cost = new IntVar(store, "cost", 0, 50); 
-   
-      
-      NetworkBuilder net = new NetworkBuilder(cost); 
-      
-      Node one = net.addNode("one", 0); 
-      Node two = net.addNode("two", 0); 
-      Node three = net.addNode("three", 0); 
-      Node four = net.addNode("four", 0); 
-      Node five = net.addNode("five", 0); 
-      Node six = net.addNode("six", 0); 
-              
-      Arc one_two = net.addArc(one, two, weights[0] ,0,1); 
-      Arc one_four= net.addArc(one,four, weights[1] ,0,1);
-      Arc one_three= net.addArc(one,three, weights[2] ,0,1);
-      Arc two_three = net.addArc(two, three, weights[3] ,0,1); 
-      Arc two_four= net.addArc(two,four, weights[4] ,0,1);
-      Arc two_five = net.addArc(two,five, weights[5] ,0,1); 
-      Arc three_five= net.addArc(three,five, weights[6] ,0,1);
-      Arc four_five = net.addArc(four, five, weights[7] ,0,1); 
-      Arc four_six= net.addArc(four,six, weights[8] ,0,1);
-      Arc five_six= net.addArc(five,six, weights[9] ,0,1);
-      
-      
- 
-      // IntVar cost = new IntVar(store, "cost", 0, 50); 
-      
-      
-       //net.setCostVariable (cost);
-        
- 
-      //store.impose( NetworkFlow const = new NetworkFlow(net));
   
-    //hey
+      
     
 }
